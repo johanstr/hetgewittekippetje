4b0832ecc69c51c5a11b9ccdf3e3b5de8165987e
The Sick Dude <jj.strootman@alfa-college.nl>
2019-10-07 16:21:29 +0200

    Startbestanden voor Praktijkopdracht 1 - API

---
+++ README.md
@@ -0,0 +1,13 @@
+# API's - Praktijkopdracht
+## Het gewitte kippetje
+
+### Inleiding
+Je vindt hier de startcode van de client. Voor de client-side schrijf je je eigen code in ***index.js***.
+
+### Wat moet je doen?
+Je schrijft in JavaScript de code waarmee je een online API aanroept om alle data te krijgen zodat je de pagina kunt vullen. De pagina moet uiteindelijk een overzicht geven van alle producten die de webshop aan kan bieden.  
+  
+Het resultaat zal dan moeten gelijken op:  
+  
+![Resultaat](https://hetgewittekippetje.ao-alfa.nl/img/resultaat.png "Het resultaat als je het goed gedaan hebt")  
+  
---
+++ js/index.js
@@ -0,0 +1,23 @@
+/*
+    HIERONDER VIND JE DE CODE VAN EEN BLOK IN DE VORM VAN EEN JAVASCRIPT TEMPLATE
+    DEZE MAG JE GEBRUIKEN VOOR HET OPBOUWEN VAN DE PAGINA
+    LET OP: NEEM OOK DE BACKTICKS MEE
+    ------------------------------------------------------------------------------
+
+        `<div class="product col-md-3">
+            <img src="img/product01.jpg" class="product-img-top" />
+            <div class="product-body">
+                <h5 class="product-title">Bruine kip</h5>
+                <p class="product-text">Geschikt voor alle wedstrijddeelnemers.</p>
+                <p class="product-price-tag">&euro;&nbsp;29,95</p>
+            </div>
+        </div>`
+
+    -----------------------------------------------------------------------------
+    HIERBOVEN STAAT DUS DE CODE
+*/
+
+/******************************************************************************** 
+    SCHRIJF HIERONDER JOUW CODE.
+    UPLOAD DIT BESTAND DAN IN DE MAP DIE JE VIA DE MAIL HEBT ONTVANGEN.
+*********************************************************************************/
